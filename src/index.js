f65a3e1be61c1d360c73261829cb860345c80264
rkonesky <rosskon1023@gmail.com>
2022-01-24 20:59:30 -0600

    frontend: added basic component and page structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Import Styles
 import './index.css';
+// Import Router
+import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
