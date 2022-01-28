3133f77a9f705ded7f7cc2e6beeddda3964470b0
rkonesky <rosskon1023@gmail.com>
2022-01-27 21:19:41 -0600

    frontend: styling changes and custom form introduction

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@
 function Footer(props) {
     return (
         <div className="footer">
-            <div>Copyright &copy; All Rights Reserved || 2021 Popcorn Time</div>
+            <div>Copyright &copy; All Rights Reserved || 2022 Popcorn Time</div>
             <div>Powered by TMDB API</div>
         </div>
     )
--- src/pages/Trailers.js
+++ src/pages/Trailers.js
@@ -126,7 +126,124 @@ function Trailers(props) {
                     />
                 </form>
             </div>
-            {/* Placeholder for Genre Search */}
+            <div className="trailers-search">
+                <div className="trailers-search-info">
+                    <h2>Custom search for trailers, by date of release, actor and genre!</h2>
+                </div>
+                <form className="trailers-search-form">
+                    <div className="date-search">
+                        <label for="datemin">Release Date start:</label>
+                        <input 
+                            type="date" 
+                            id="datemin" 
+                            name="datemin" 
+                            min="1900-01-01"
+                            max="2024-01-01"
+                        />
+                        <label for="datemax">To:</label>
+                        <input 
+                            type="date" 
+                            id="datemax" 
+                            name="datemax" 
+                            min="1900-01-01"
+                            max="2024-01-01"
+                        />
+                    </div>
+                    <div className="actor-search">
+                        <label for="actor">Features actor:</label>
+                        <input
+                            type="text"
+                            id="actor"
+                            value=""
+                            name="radio-buttons"
+                        />
+                    </div>
+                    <div className="genre-search">
+                        <label for="genre">Select Genre:</label>
+                        <select id="genre" name="genre">
+                            <option value="">Genres</option>
+                            <option value="Thriller">Thriller</option>
+                            <option value="Animation">Animation</option>
+                            <option value="Sci Fi">Sci Fi</option>
+                            <option value="Action">Action</option>
+                            <option value="Comedy">Comedy</option>
+                            <option value="Drama">Drama</option>
+                            <option value="Horror">Horror</option>
+                            <option value="Crime">Crime</option>
+                            <option value="Romance">Romance</option>
+                        </select>
+                    </div>
+                    {/* <div className="genre-search">
+                        <p>Find trailers by genre</p>
+                        <input
+                            type="radio"
+                            id="Thriller"
+                            name="radio-buttons"
+                            value="Thriller"
+                        />
+                        <label for="Thriller">Thriller</label>
+                        <input
+                            type="radio"
+                            id="Animation"
+                            name="radio-buttons"
+                            value="Animation"
+                        />
+                        <label for="Animation">Animation</label>
+                        <input
+                            type="radio"
+                            id="Sci Fi"
+                            name="radio-buttons"
+                            value="Sci Fi"
+                        />
+                        <label for="Sci Fi">Sci Fi</label>
+                        <input
+                            type="radio"
+                            id="Action"
+                            name="radio-buttons"
+                            value="Action"
+                        />
+                        <label for="Action">Action</label>
+                        <input
+                            type="radio"
+                            id="Comedy"
+                            name="radio-buttons"
+                            value="Comedy"
+                        />
+                        <label for="Comedy">Comedy</label>
+                        <input
+                            type="radio"
+                            id="Drama"
+                            name="radio-buttons"
+                            value="Drama"
+                        />
+                        <label for="Drama">Drama</label>
+                        <input
+                            type="radio"
+                            id="Horror"
+                            name="radio-buttons"
+                            value="Horror"
+                        />
+                        <label for="Horror">Horror</label>
+                        <input
+                            type="radio"
+                            id="Crime"
+                            name="radio-buttons"
+                            value="Crime"
+                        />
+                        <label for="Crime">Crime</label>
+                        <input
+                            type="radio"
+                            id="Romance"
+                            name="radio-buttons"
+                            value="Romance"
+                        />
+                        <label for="Romance">Romance</label>
+                    </div> */}
+                    <div className="trailers-search-submit">
+                        <input type="submit" value="Search Trailers" />
+                    </div>
+                </form>
+            </div>
             {currentMovie ? loaded() : loading()}
         </div>
     )
@@ -135,106 +252,106 @@ function Trailers(props) {
 export default Trailers;
 
 
-            // {/* <div className="trailers-search">
-            //     <form>
-            //         <div className="date-search">
-            //             <p>Find trailers by date of release</p>
-            //             <label for="datemin">Start date:</label>
-            //             <input 
-            //                 type="date" 
-            //                 id="datemin" 
-            //                 name="datemin" 
-            //                 min="1900-01-01"
-            //                 max="2024-01-01"
-            //             />
-            //             <label for="datemax">End date:</label>
-            //             <input 
-            //                 type="date" 
-            //                 id="datemax" 
-            //                 name="datemax" 
-            //                 min="1900-01-01"
-            //                 max="2024-01-01"
-            //             />
-            //         </div>
-            //         <div className="actor-search">
-            //             <p>Find trailers by featured actors</p>
-            //             <label for="actor">Actor</label>
-            //             <input
-            //                 type="text"
-            //                 id="actor"
-            //                 value=""
-            //                 name="radio-buttons"
-            //                 placeholder="actor"
-            //             />
-            //         </div>
-            //         <div className="genre-search">
-            //             <p>Find trailers by genre</p>
-            //             <input
-            //                 type="radio"
-            //                 id="Thriller"
-            //                 name="radio-buttons"
-            //                 value="Thriller"
-            //             />
-            //             <label for="Thriller">Thriller</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Animation"
-            //                 name="radio-buttons"
-            //                 value="Animation"
-            //             />
-            //             <label for="Animation">Animation</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Sci Fi"
-            //                 name="radio-buttons"
-            //                 value="Sci Fi"
-            //             />
-            //             <label for="Sci Fi">Sci Fi</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Action"
-            //                 name="radio-buttons"
-            //                 value="Action"
-            //             />
-            //             <label for="Action">Action</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Comedy"
-            //                 name="radio-buttons"
-            //                 value="Comedy"
-            //             />
-            //             <label for="Comedy">Comedy</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Drama"
-            //                 name="radio-buttons"
-            //                 value="Drama"
-            //             />
-            //             <label for="Drama">Drama</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Horror"
-            //                 name="radio-buttons"
-            //                 value="Horror"
-            //             />
-            //             <label for="Horror">Horror</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Crime"
-            //                 name="radio-buttons"
-            //                 value="Crime"
-            //             />
-            //             <label for="Crime">Crime</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Romance"
-            //                 name="radio-buttons"
-            //                 value="Romance"
-            //             />
-            //             <label for="Romance">Romance</label>
-            //         </div>
-            //         <div className="trailers-search-submit">
-            //             <input type="submit" value="Search Trailers" />
-            //         </div>
-            //     </form>
-            // </div> */}
+            <div className="trailers-search">
+                <form>
+                    <div className="date-search">
+                        <p>Find trailers by date of release</p>
+                        <label for="datemin">Start date:</label>
+                        <input 
+                            type="date" 
+                            id="datemin" 
+                            name="datemin" 
+                            min="1900-01-01"
+                            max="2024-01-01"
+                        />
+                        <label for="datemax">End date:</label>
+                        <input 
+                            type="date" 
+                            id="datemax" 
+                            name="datemax" 
+                            min="1900-01-01"
+                            max="2024-01-01"
+                        />
+                    </div>
+                    <div className="actor-search">
+                        <p>Find trailers by featured actors</p>
+                        <label for="actor">Actor</label>
+                        <input
+                            type="text"
+                            id="actor"
+                            value=""
+                            name="radio-buttons"
+                            placeholder="actor"
+                        />
+                    </div>
+                    <div className="genre-search">
+                        <p>Find trailers by genre</p>
+                        <input
+                            type="radio"
+                            id="Thriller"
+                            name="radio-buttons"
+                            value="Thriller"
+                        />
+                        <label for="Thriller">Thriller</label>
+                        <input
+                            type="radio"
+                            id="Animation"
+                            name="radio-buttons"
+                            value="Animation"
+                        />
+                        <label for="Animation">Animation</label>
+                        <input
+                            type="radio"
+                            id="Sci Fi"
+                            name="radio-buttons"
+                            value="Sci Fi"
+                        />
+                        <label for="Sci Fi">Sci Fi</label>
+                        <input
+                            type="radio"
+                            id="Action"
+                            name="radio-buttons"
+                            value="Action"
+                        />
+                        <label for="Action">Action</label>
+                        <input
+                            type="radio"
+                            id="Comedy"
+                            name="radio-buttons"
+                            value="Comedy"
+                        />
+                        <label for="Comedy">Comedy</label>
+                        <input
+                            type="radio"
+                            id="Drama"
+                            name="radio-buttons"
+                            value="Drama"
+                        />
+                        <label for="Drama">Drama</label>
+                        <input
+                            type="radio"
+                            id="Horror"
+                            name="radio-buttons"
+                            value="Horror"
+                        />
+                        <label for="Horror">Horror</label>
+                        <input
+                            type="radio"
+                            id="Crime"
+                            name="radio-buttons"
+                            value="Crime"
+                        />
+                        <label for="Crime">Crime</label>
+                        <input
+                            type="radio"
+                            id="Romance"
+                            name="radio-buttons"
+                            value="Romance"
+                        />
+                        <label for="Romance">Romance</label>
+                    </div>
+                    <div className="trailers-search-submit">
+                        <input type="submit" value="Search Trailers" />
+                    </div>
+                </form>
+            </div>
