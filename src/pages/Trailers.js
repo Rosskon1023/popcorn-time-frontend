d770dc831aee98ff8412612f5e26a451f8500ae3
rkonesky <rosskon1023@gmail.com>
2022-01-31 20:58:25 -0600

    frontend: fixed bug that prevented search for certain actors where videoKey was missing

--- src/pages/Trailers.js
+++ src/pages/Trailers.js
@@ -90,19 +90,24 @@ function Trailers(props) {
             for (const movieId of searchMovies) {
                 const movieIdResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&append_to_response=videos`)
                 const movieIdData = await movieIdResponse.json()
-                const movieIdVideo = movieIdData.videos.results;
                 let array = [];
-                for (let i=0; i<movieIdVideo.length; i++) {
-                    if(movieIdVideo[i].name.includes("Official") && movieIdVideo[i].type.includes("Trailer")) {
-                        let videoKey = movieIdVideo[i].key;
-                        array.push({movieIdData,videoKey})
-                        break;
-                    } else if (movieIdVideo[i].type.includes("Trailer")) {
-                        let videoKey = movieIdVideo[i].key;
-                        array.push({movieIdData,videoKey})
-                        break;
+                if (movieIdData.videos.results.length < 1) {
+                    break;
+                } else {
+                    const movieIdVideo = movieIdData.videos.results;
+                    for (let i=0; i<movieIdVideo.length; i++) {
+                        if(movieIdVideo[i].name.includes("Official") && movieIdVideo[i].type.includes("Trailer")) {
+                            let videoKey = movieIdVideo[i].key;
+                            array.push({movieIdData,videoKey})
+                            break;
+                        } else if (movieIdVideo[i].type.includes("Trailer")) {
+                            let videoKey = movieIdVideo[i].key;
+                            array.push({movieIdData,videoKey})
+                            break;
+                        }
                     }
                 }
+                
                 stateArray.push(array);
             }
             setMovies(stateArray);
@@ -321,108 +326,3 @@ function Trailers(props) {
 }
 
 export default Trailers;
-
-
-            // <div className="trailers-search">
-            //     <form>
-            //         <div className="date-search">
-            //             <p>Find trailers by date of release</p>
-            //             <label for="datemin">Start date:</label>
-            //             <input 
-            //                 type="date" 
-            //                 id="datemin" 
-            //                 name="datemin" 
-            //                 min="1900-01-01"
-            //                 max="2024-01-01"
-            //             />
-            //             <label for="datemax">End date:</label>
-            //             <input 
-            //                 type="date" 
-            //                 id="datemax" 
-            //                 name="datemax" 
-            //                 min="1900-01-01"
-            //                 max="2024-01-01"
-            //             />
-            //         </div>
-            //         <div className="actor-search">
-            //             <p>Find trailers by featured actors</p>
-            //             <label for="actor">Actor</label>
-            //             <input
-            //                 type="text"
-            //                 id="actor"
-            //                 value=""
-            //                 name="radio-buttons"
-            //                 placeholder="actor"
-            //             />
-            //         </div>
-            //         <div className="genre-search">
-            //             <p>Find trailers by genre</p>
-            //             <input
-            //                 type="radio"
-            //                 id="Thriller"
-            //                 name="radio-buttons"
-            //                 value="Thriller"
-            //             />
-            //             <label for="Thriller">Thriller</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Animation"
-            //                 name="radio-buttons"
-            //                 value="Animation"
-            //             />
-            //             <label for="Animation">Animation</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Sci Fi"
-            //                 name="radio-buttons"
-            //                 value="Sci Fi"
-            //             />
-            //             <label for="Sci Fi">Sci Fi</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Action"
-            //                 name="radio-buttons"
-            //                 value="Action"
-            //             />
-            //             <label for="Action">Action</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Comedy"
-            //                 name="radio-buttons"
-            //                 value="Comedy"
-            //             />
-            //             <label for="Comedy">Comedy</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Drama"
-            //                 name="radio-buttons"
-            //                 value="Drama"
-            //             />
-            //             <label for="Drama">Drama</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Horror"
-            //                 name="radio-buttons"
-            //                 value="Horror"
-            //             />
-            //             <label for="Horror">Horror</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Crime"
-            //                 name="radio-buttons"
-            //                 value="Crime"
-            //             />
-            //             <label for="Crime">Crime</label>
-            //             <input
-            //                 type="radio"
-            //                 id="Romance"
-            //                 name="radio-buttons"
-            //                 value="Romance"
-            //             />
-            //             <label for="Romance">Romance</label>
-            //         </div>
-            //         <div className="trailers-search-submit">
-            //             <input type="submit" value="Search Trailers" />
-            //         </div>
-            //     </form>
-            // </div>
